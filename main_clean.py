3d7a385a009c0c4f0a16c99086cbcf87878c9d58
Moni286 <mke255@nyu.edu>
2018-05-03 20:41:16 +0400

    Cleaned up main_clean.py

--- main_clean.py
+++ main_clean.py
@@ -54,7 +54,7 @@ def main():
 			
 			if(mode == 2) :			
 				if not robotFound or target == None:
-					print "ROBOT NOT FOUND"
+					print "ROBOT OR TARGET NOT FOUND"
 					myVehicle.stop()
 					continue
 			elif (mode == 3):
@@ -69,25 +69,19 @@ def main():
 				dist = get_distance(robot.xpos, robot.ypos, target.xpos, target.ypos)
 
 			elif (mode == 3) :
-				#print "robot xpos = ", robot.xpos, " robot ypos = ", robot.ypos
 				req_or = get_required_orientation(robot.xpos, robot.ypos, trajectory[trajectoryindex][0], trajectory[trajectoryindex][1])
 				dist = get_distance(robot.xpos, robot.ypos, trajectory[trajectoryindex][0], trajectory[trajectoryindex][1])
 
 			cur_angle = robot.angle
 
 			if prev_x == robot.xpos and prev_y == robot.ypos and myVehicle.status != 0:
-				#print "stuck"
 				continue
 			elif cur_angle == prev_angle and myVehicle.status != 0 and myVehicle.status != 3 and myVehicle.status != 4 and prev_x == robot.xpos and prev_y == robot.ypos:
-				print "rotation stuck"
 				continue
 			else:
 				prev_angle = cur_angle
 				prev_x = robot.xpos
 				prev_y = robot.ypos
-				# print "update time: ", time() - update_time
-				# update_time = time()
-				#print "loopin"
 
 			
 			req = req_or
@@ -102,7 +96,6 @@ def main():
 			else:
 				if not myVehicle.rotate_to(cur, req, dist, 50):
 					myVehicle.curve_to(cur, req, 0, dist, 40)
-						# myVehicle.go_forward(60)
 			
 
 			if mode == 3 and dist < maxDist:
